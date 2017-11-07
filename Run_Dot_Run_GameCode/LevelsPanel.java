f433ab558d9ea9e1b2b185300f77e2d88c6b4325
BeyzaTugce <32226078+BeyzaTugce@users.noreply.github.com>
2017-11-07 17:00:34 +0200

    Add files via upload

---
+++ Run_Dot_Run_GameCode/LevelsPanel.java
@@ -0,0 +1,144 @@
+import javax.imageio.ImageIO; 
+import javax.swing.*; 
+import java.awt.*; 
+import java.awt.image.BufferedImage; 
+import java.io.File; 
+import java.io.IOException; 
+import javax.swing.border.*;
+
+
+public class LevelsPanel extends JPanel{ 
+  
+  ImageIcon imgLocked;
+  ImageIcon imgUnlocked;
+  ImageIcon img;
+  JLabel unlocked;
+  JLabel locked;
+  JLabel levels;
+  JLabel num;
+  JPanel top;
+  JPanel middle;
+  JPanel bottom;
+  JPanel levelNum;
+  JButton back;
+  
+  public LevelsPanel(){
+    
+    top = new JPanel();
+    top.setPreferredSize(new Dimension(1000, 100)); 
+    top.setBackground(Color.PINK);
+    top.setLayout( new BoxLayout( top, BoxLayout.X_AXIS));
+    
+    middle = new JPanel();
+    middle.setPreferredSize(new Dimension(1000, 200)); 
+    middle.setBackground(Color.PINK);
+    levels = new JLabel("LEVELS");
+    levels.setFont(new Font("CALIBRI", Font.BOLD | Font.ITALIC, 70));
+    
+    bottom = new JPanel();
+    bottom.setPreferredSize(new Dimension(1000, 200));
+    bottom.setBackground(Color.PINK);
+    
+    levelNum = new JPanel();
+    levelNum.setPreferredSize(new Dimension(1000, 200));
+    levelNum.setBackground(Color.PINK);
+    JLabel num1 = new JLabel("Level 1");
+    num1.setFont(new Font("CALIBRI", Font.BOLD, 30));
+    JLabel num2 = new JLabel("Level 2");
+    num2.setFont(new Font("CALIBRI", Font.BOLD, 30));
+    JLabel num3 = new JLabel("Level 3");
+    num3.setFont(new Font("CALIBRI", Font.BOLD, 30));
+    JLabel num4 = new JLabel("Level 4");
+    num4.setFont(new Font("CALIBRI", Font.BOLD, 30));
+    
+    
+    //Unlocked level
+    img = new ImageIcon( getClass().getResource("unlocked.png"));
+    Image temp = img.getImage();
+    temp = temp.getScaledInstance(200, 200,  java.awt.Image.SCALE_SMOOTH);
+    imgUnlocked = new ImageIcon(temp);   
+    unlocked = new JLabel(imgUnlocked);
+    
+    //Locked level
+    img = new ImageIcon( getClass().getResource("locked.png"));
+    temp = img.getImage();
+    temp = temp.getScaledInstance(200, 200,  java.awt.Image.SCALE_SMOOTH);
+    imgLocked = new ImageIcon(temp);   
+    locked = new JLabel(imgLocked);
+    
+    //Back button
+    img = new ImageIcon( getClass().getResource("back.png"));
+    temp = img.getImage();
+    temp = temp.getScaledInstance(50, 50,  java.awt.Image.SCALE_SMOOTH);
+    img = new ImageIcon(temp);   
+    back = new JButton(img);
+    
+    unlocked.setOpaque(false);
+    locked.setOpaque(false);
+    back.setOpaque(false);
+    
+    
+    
+    //Add components to panel
+    top.add(Box.createRigidArea( new Dimension(30, 0)));
+    top.add(back);
+    middle.add( levels);
+    bottom.add( unlocked);
+    bottom.add( locked);
+    bottom.add( copyComponent("locked"));
+    bottom.add( copyComponent("locked"));
+    
+    levelNum.add(num1);
+    levelNum.add(Box.createRigidArea( new Dimension(70, 0)));
+    levelNum.add(num2);
+    levelNum.add(Box.createRigidArea( new Dimension(70, 0)));
+    levelNum.add(num3);
+    levelNum.add(Box.createRigidArea( new Dimension(70, 0)));
+    levelNum.add(num4);
+    
+    this.setPreferredSize(new Dimension(1000, 700)); 
+    this.setLayout( new FlowLayout());
+    this.setBackground(Color.PINK);
+    this.add(top);
+    this.add(middle);
+    this.add(bottom);
+    this.add(levelNum);
+    
+  }
+  
+  //Make a copy of level components
+  public JLabel copyComponent( String lock){
+    
+    JLabel temp = null;  
+    if ( lock.equals("locked")){
+      temp = new JLabel(imgLocked);  
+    }
+    else if ( lock.equals("unlocked")){
+      temp = new JLabel(imgUnlocked); 
+    } 
+    return temp;
+  }
+  
+  //Change to lock or unlock
+  public void changeStatus(JLabel label, String lock){
+    
+    if ( lock.equals("locked")){
+      label = new JLabel(imgUnlocked);  
+    }
+    else if ( lock.equals("unlocked")){
+      label = new JLabel(imgLocked); 
+    } 
+  }
+  
+  public static void main( String[] args )
+  {
+    JFrame mainMenuFrame = new JFrame();
+    mainMenuFrame.add( new LevelsPanel() );
+    mainMenuFrame.setSize(1000, 700); 
+    mainMenuFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+    mainMenuFrame.setResizable(false); 
+    mainMenuFrame.setLocationRelativeTo(null); 
+    mainMenuFrame.setVisible(true);
+    mainMenuFrame.pack();
+  }     
+}
